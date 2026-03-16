4a3082f9423aed0eba35d167a0e71286aedd6523
RaresStefea <Rares.Stefea@cognizant.com>
2026-03-16 14:37:48 +0200

    added config and postgresql.

--- utils/jwt.py
+++ utils/jwt.py
@@ -1,15 +1,13 @@
-import os
 from datetime import datetime, timedelta, timezone
 from jose import jwt
-from dotenv import load_dotenv
+from config.settings import settings
 
 # TIP: load_env should always run before trying to access the env vars so it gets injected and the os can retrieve it
-if os.getenv("ENV") != "production":
-    load_dotenv()
+# if settings.settings.env_mode != "production":
+#    load_dotenv()
 
-SECRET_KEY = os.getenv("JWT_SECRET_KEY")
 
-if not SECRET_KEY:
+if not settings.jwt_secret_key:
     raise RuntimeError("JWT_SECRET_KEY missing, check env vars.")
 
 ALGORITHM = "HS256"
@@ -19,12 +17,12 @@ EXPIRE_MINUTES = 60 * 24
 def create_access_token(user_id: int) -> str:
     expire = datetime.now(timezone.utc) + timedelta(minutes=EXPIRE_MINUTES)
     payload = {"sub": str(user_id), "exp": int(expire.timestamp())}
-    return jwt.encode(payload, SECRET_KEY, algorithm=ALGORITHM)
+    return jwt.encode(payload, settings.jwt_secret_key, algorithm=ALGORITHM)
 
 
 def decode_access_token(token: str) -> dict:
     try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        payload = jwt.decode(token, settings.jwt_secret_key, algorithms=[ALGORITHM])
         user_id = payload.get("sub")
         if user_id is None:
             return None
